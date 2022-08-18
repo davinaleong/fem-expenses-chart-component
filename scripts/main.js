9309432b4ca9fddda2afd9a1d8c7111df65726b6
Davina Leong <davina.leong.sy@gmail.com>
2022-08-18 14:44:10 +0800

    Fixed errors

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,33 @@
-import data from "./data.json"
-
+const data = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+]
 const chartEl = document.querySelector(`[data-chart]`)
 
 const date = new Date()
