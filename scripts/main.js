54e63d1339a0b3cd1ed8ef9ebdefde34f831e160
Davina Leong <davina.leong.sy@gmail.com>
2022-08-18 14:30:55 +0800

    Populated chart

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,52 @@
 import data from "./data.json"
 
 const chartEl = document.querySelector(`[data-chart]`)
-const chartHeight = chartEl.clientHeight
 
+const date = new Date()
 const maxValue = Math.max(...data.map((object) => object.amount))
-console.log(`maxValue: `, maxValue)
+
+const getDay = (date) => {
+  switch (date.getDay()) {
+    case 0:
+      return `sun`
+
+    case 1:
+      return `mon`
+
+    case 2:
+      return `tue`
+
+    case 3:
+      return `wed`
+
+    case 4:
+      return `thu`
+
+    case 5:
+      return `fri`
+
+    case 6:
+      return `sat`
+
+    default:
+      return ``
+  }
+}
+
+chartEl.innerHTML = ``
+data.forEach((object) => {
+  const { day, amount } = object
+  const height = Math.round((Number(amount) / maxValue) * 100)
+
+  const today =
+    getDay(date).toLowerCase() == day.toLowerCase() ? `data-today` : ``
+
+  chartEl.innerHTML += `
+        <div class="chart-column">
+            <button class="chart-bar" ${today} style="--height: ${height}%">
+                <div class="chart-popup">$${amount}</div>
+            </button>
+            <div class="chart-label">${day}</div>
+        </div>
+    `
+})
